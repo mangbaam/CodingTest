9b6d8794b477cb54d27519bf386a4fdbcdac2201
박명범 <44221447+mangbaam@users.noreply.github.com>
2022-08-19 20:24:44 +0900

    [Gold IV] Title: 오큰수, Time: 592 ms, Memory: 260516 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+n = int(input())
+nums = list(map(int, input().split()))
+answer = [-1] * n
+stack = list()
+
+for i in range(n):
+    while stack and nums[stack[-1]] < nums[i]:
+        answer[stack.pop()] = nums[i]
+    stack.append(i)
+
+print(*answer, sep=' ')
