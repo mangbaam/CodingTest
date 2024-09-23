967dc28bb7ad6b723120be82f660dac96a7c5497
박명범 <44221447+mangbaam@users.noreply.github.com>
2024-09-24 06:49:05 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int chicken = sc.nextInt();
+        if (chicken * 2 <= A + B) System.out.print(A + B - chicken * 2);
+        else System.out.print(A + B);
+    }
+}
