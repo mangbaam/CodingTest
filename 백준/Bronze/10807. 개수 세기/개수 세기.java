4e71b7afd6ee6389b49bd173f3825973b0135ac8
박명범 <44221447+mangbaam@users.noreply.github.com>
2024-09-24 02:48:37 +0900

    [Bronze V] Title: 개수 세기, Time: 176 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int v = sc.nextInt();
+        System.out.println(Arrays.stream(nums).filter(n -> n == v).count());
+    }
+}
